1f53918ac43872d2aab205cc0cd6a8776404f7aa
leclair-7 <andromeda.t@tutanota.com>
2018-05-26 11:23:17 -0500

    Improved sobel operator function, and removed the HSV sobel function because operations were too coupled

--- radiusOfCurvature.py
+++ radiusOfCurvature.py
@@ -19,4 +19,29 @@ def radiusOfCurvature(point):
 
 	return r(point)
 
+def getCenterPoint(point):
+
+
+	m2 = -1. / y_prime
+
+
+'''
 print(radiusOfCurvature(1))
+
+a = lambda x: -1* x / 5 + 21./5
+print(a(-9.83))
+
+print(a(11.84))
+'''
+
+from sympy import *
+import numpy as np
+x = Symbol('x')
+y = 2 * x**3 - x + 3
+y_prime = diff(y,x)
+y_2prime = diff(y,x,2)
+
+print(y_prime)
+print(y_2prime)
+print(y_prime.subs(x,1) )
+print(y_2prime.subs(x,2))
